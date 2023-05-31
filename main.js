59b9e9ed038d16081132057702664d967b2e6ff9
Ghost-Uchiha <niravjangale@gmail.com>
2023-05-31 18:15:25 +0530

    Complete

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const questions = [
 	{ question: "Do you have good communication skills ?", value: ["Marketing","HR"] },
 	{ question: "Are you an innovative thinker ?", value: ["Marketing"] },
-	{ question: "Are you good with huge Data/numbers ?", value: ["Finance"] },
+	{ question: "Are you good with huge data/numbers ?", value: ["Finance"] },
 	{ question: "Are you willing to learn the basics of the specialisation before joining that specialisation ?", value: ["Finance"] },
 	{ question: "Do you make any effort necessary to accomplish the task?", value: ["Marketing"] },
-	{ question: "Are you good with reasearch on the internet ?", value: ["Marketing"] },
-	{ question: "Are you comfortable working with teams ? /Are you a team player ?", value: ["Marketing","HR"] },
-	{ question: "Do you love coachng others ? Do you love training others ?", value: ["HR"] },
+	{ question: "Are you good at conducting reasearch on the internet ?", value: ["Marketing"] },
+	{ question: "Are you comfortable working with teams ? Are you a team player ?", value: ["Marketing","HR"] },
+	{ question: "Do you love coaching others ? Do you love training others ?", value: ["HR"] },
 	{ question: "Do you remain cool under pressure ?", value: ["Finance"] },
-	{ question: "Can you use predictive analysis ? /Can you use logic and connect the dots between events ?", value: ["Finance"] },
+	{ question: "Can you use predictive analysis ? Can you use logic and connect the dots between events ?", value: ["Finance"] },
 	{ question: "Are you good at critical/analytical thinking ?", value: ["Finance","Operations","IT"] },
-	{ question: "Are you good at even organisation ?", value: ["HR"] },
+	{ question: "Are you good at event organisation/management ?", value: ["HR"] },
 	{ question: "Are you good at monitoring and supervision ?", value: ["HR","Operations","IT"] },
 	{ question: "Are you a good planner ?", value: ["Operations","IT"] },
 	{ question: "Do you easily adapt to the situations at hand ?", value: ["Operations","IT"] },
@@ -18,17 +18,17 @@ const questions = [
 	{ question: "Are you computer literate ?", value: ["IT"] },
 	{ question: "Do you have good leadership skills ?", value: ["Finance","Operations","IT","HR","Marketing"] },
 	{ question: "Do you take quick decisions ?", value: ["Finance","Operations","IT","HR","Marketing"] },
-	{ question: "Do you have an entrepreneurial spirit ?/ Do you like to take initiatives ?", value: ["Finance","Operations","IT","HR","Marketing"] }
+	{ question: "Do you have an entrepreneurial spirit ? Do you like to take initiatives ?", value: ["Finance","Operations","IT","HR","Marketing"] }
 
   ];
   
   
   const categories = [
-	{ name: "Marketing", description: "Marketers are responsible for planning, creating, and executing marketing campaigns to expand their company’s reach and potential customer pipeline. A marketing job description should include the necessary qualifications, responsibilities, and skills required for success in the role.'", image: "image/Marketing.jpeg" },
-	{ name: "Finance", description: "Coordinate and execute financial transactions and activities, such as bill payment, invoicing, payroll, etc. Assist in the audit process by providing information and data as requested. Complete administrative tasks, such as filling out paperwork and filing records.", image: "image/Finance.jpeg" },
-	{ name: "Operations", description: "Operations Managers oversee operational activities at every level of an organization. Their duties include hiring and training employees and managing quality assurance programs. An operations manager also strategizes process improvements to ensure everyone completes their tasks on schedule.", image: "image/operations.jpeg" },
-	{ name: "IT", description: "An IT position generally involves managing and storing data using computers, software, databases, networks, and servers. As an IT professional, you may write programs, maintain networks, analyze systems, and provide technical support.", image: "image/IT.jpeg" },
-	{ name: "HR", description: "A Human Resources (HR) Officer is responsible for managing every aspect of the employment process, including orientation and training new staff members. They also assist with payroll management, so employees receive their paychecks on time.", image: "image/HR.jpg" },
+	{ name: "Marketing", description: "A Marketing MBA is a specialized graduate degree that focuses on developing expertise in marketing strategy, consumer behaviour, market research, and brand management. This program combines core business principles with specialized marketing courses to equip individuals with the knowledge and skills necessary to navigate the dynamic world of marketing. A Marketing MBA curriculum typically covers areas such as marketing analytics, digital marketing, product development, and marketing communications. Graduates of a Marketing MBA program often pursue careers in advertising, brand management, market research, or marketing consulting. This degree prepares professionals to create effective marketing strategies, build strong brands, and drive business growth in today&#39;s competitive marketplaces.", image: "image/Marketing.jpg" },
+	{ name: "Finance", description: "A Finance MBA is a specialized graduate degree that equips individuals with the knowledge and skills needed to excel in the field of finance. It combines the principles of business administration with a deep understanding of financial analysis, investment management, and risk assessment. A Finance MBA program typically covers a wide range of topics such as	financial planning, corporate finance, derivatives, and portfolio management. Graduates of a Finance MBA program often pursue careers in investment banking, corporate finance,financial consulting, or asset management. This degree provides a solid foundation for professionals seeking to make strategic financial decisions and drive organizational success in today&#39;s complex financial landscape.", image: "image/Finance.jpg" },
+	{ name: "Operations", description: "An Operations MBA is a specialized graduate degree that focuses on the management and optimization of business operations. This program combines core business principles with specialized coursework in operations management, supply chain management, logistics,process improvement, and project management. An Operations MBA equips individuals with the knowledge and skills necessary to streamline processes, increase efficiency, and maximize productivity within organizations. Graduates of an Operations MBA program often pursue careers as operations managers, supply chain analysts, project managers, or consultants. This degree prepares professionals to effectively manage resources, drive operational excellence, and contribute to the overall success of organizations in today&#39;s fast-paced and competitive business environment.", image: "image/operations.jpg" },
+	{ name: "IT", description: "An IT MBA, or Information Technology MBA, is a specialized graduate degree that focuses on the intersection of business and technology. This program combines core business administration principles with specialized coursework in areas such as IT strategy, data analytics, cybersecurity, project management, and digital transformation. An IT MBA equips individuals with the knowledge and skills needed to lead and manage IT initiatives within organizations. Graduates of an IT MBA program often pursue careers as IT managers, technology consultants, data analysts, or business analysts. This degree prepares professionals to bridge the gap between technology and business objectives, leverage emerging technologies, and drive innovation and digital strategies in today&#39;s technology-driven business landscape.", image: "image/IT.jpg" },
+	{ name: "HR", description: "An HR MBA, or Human Resources MBA, is a specialized graduate degree designed to develop professionals with advanced knowledge and skills in human resource management. This program focuses on the strategic aspects of HR, including talent acquisition, employee development, performance management, compensation and benefits, and organizational behaviour. HR MBAs blend business administration principles with specialized HR courses to equip individuals with the expertise necessary to navigate the complexities of managing human capital in organizations. Graduates of an HR MBA program often pursue careers as HR managers, talent acquisition specialists, training and development managers, or HR consultants. This degree prepares professionals to effectively lead and support the people management function within organizations and drive employee engagement and organizational success.", image: "image/HR.jpg" },
 
 ];
   
@@ -66,8 +66,9 @@ const questions = [
     if (currentQuestionIndex >= questions.length) {
         document.getElementById('question').innerHTML = "";
         number.textContent = "";
-        document.getElementById('main-header').innerHTML = "ENTER YOUR EMAIL AND PHONE NUMBER FOR SEE THE RESULT  ";
+        document.getElementById('main-header').innerHTML = "ENTER YOUR EMAIL AND PHONE NUMBER TO SEE THE RESULT  ";
         document.getElementById('detail').innerHTML = "";
+		document.getElementById('body').style.removeProperty('overflow')
         // Get all elements with the class name 'hide-button'
 var buttons = document.getElementsByClassName('bt');
 
@@ -85,6 +86,7 @@ for (var i = 0; i < buttons.length; i++) {
        
         document.getElementById('req').style.border = "1px solid #000";
         document.getElementById('req').style.borderRadius = "10px";
+		
     }
 
     currentQuestion = questions[currentQuestionIndex];
@@ -94,43 +96,173 @@ for (var i = 0; i < buttons.length; i++) {
 
   
   
-  
-  
-  function display() {
-	
+function display() {
     const ph = document.getElementById("ph").value.trim();
     const gmail = document.getElementById("gmail").value.trim();
-    
+
     if (ph === "" || gmail === "") {
-        alert("Please enter your name and Email to see the results.");
+        alert("Please enter your phone number and email to see the results.");
         return;
     }
-	
-	if (!isValidEmail(gmail)) {
-		alert("Please enter a valid Email address.");
-		return;
+
+    if (!isValidEmail(gmail)) {
+        alert("Please enter a valid email address.");
+        return;
+    }
+
+    addPhoneNumber(ph);
+    addEmail(gmail);
+	displayResults()
+}
+document.addEventListener("DOMContentLoaded", function(){
+	document.getElementById("ph").addEventListener("focus", showDropdownph);
+document.getElementById("ph").addEventListener("blur", showDropdownph);
+
+})
+	function showDropdownph() {
+		const input = document.getElementById("ph");
+    const dropdown = document.getElementById("previousPhoneNumbers");
+	if (input.value.trim() !== "") {
+		dropdown.style.display = "block";
+	} else {
+		dropdown.style.display = "none";
 	}
-		displayResults()
+}
+document.addEventListener("DOMContentLoaded", function(){
+	document.getElementById("gmail").addEventListener("focus", showDropdownemail);
+document.getElementById("gmail").addEventListener("blur", showDropdownemail);
+
+})
+	function showDropdownemail() {
+		const input = document.getElementById("gmail");
+    const dropdown = document.getElementById("previousEmails");
+		if (input.value.trim() !== "") {
+			dropdown.style.display = "block";
+		} else {
+			dropdown.style.display = "none";
+		}
 	
-    
-    
+		
+}
+function addPhoneNumber(number) {
+    let phoneNumbers = getPhoneNumbers();
+
+    // Check if the number already exists
+    if (phoneNumbers.includes(number)) {
+        return; // Don't add the number again
+    }
+
+    phoneNumbers.push(number);
+
+    const selectPhone = document.getElementById("previousPhoneNumbers");
+    const option = document.createElement("option");
+    option.text = number;
+    selectPhone.add(option);
+
+    // Store the phone numbers in localStorage
+    localStorage.setItem("phoneNumbers", JSON.stringify(phoneNumbers));
+}
+
+function addEmail(email) {
+    let emails = getEmails();
+
+    // Check if the email already exists
+    if (emails.includes(email)) {
+        return; // Don't add the email again
+    }
+
+    emails.push(email);
+
+    const selectEmail = document.getElementById("previousEmails");
+    const option = document.createElement("option");
+    option.text = email;
+    selectEmail.add(option);
+
+    // Store the emails in localStorage
+    localStorage.setItem("emails", JSON.stringify(emails));
+}
+
+
+function loadPreviousValues() {
+    const phoneNumbers = getPhoneNumbers();
+    const emails = getEmails();
+
+    const selectPhone = document.getElementById("previousPhoneNumbers");
+    const selectEmail = document.getElementById("previousEmails");
+
+    // Clear previous options
+    selectPhone.innerHTML = "";
+    selectEmail.innerHTML = "";
+
+    phoneNumbers.forEach((number) => {
+        const option = document.createElement("option");
+        option.text = number;
+        selectPhone.add(option);
+    });
+
+    emails.forEach((email) => {
+        const option = document.createElement("option");
+        option.text = email;
+        selectEmail.add(option);
+    });
+}
+
+
+function getPhoneNumbers() {
+    const phoneNumbers = localStorage.getItem("phoneNumbers");
+    return phoneNumbers ? JSON.parse(phoneNumbers) : [];
+}
+
+function getEmails() {
+    const emails = localStorage.getItem("emails");
+    return emails ? JSON.parse(emails) : [];
 }
 
 function isValidEmail(email) {
-	// Regular expression to validate the email format
-	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-	return emailRegex.test(email);
+    // Add your email validation logic here
+    // This is a basic email validation using regular expression
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
 }
-function restrictPhoneNumberInput(event) {
-    const charCode = event.which ? event.which : event.keyCode;
 
+function fillPhoneNumber() {
+    const selectPhone = document.getElementById("previousPhoneNumbers");
+    const phoneNumberInput = document.getElementById("ph");
+    const selectedPhoneNumber = selectPhone.options[selectPhone.selectedIndex].text;
+    phoneNumberInput.value = selectedPhoneNumber;
+}
+
+function fillEmail() {
+    const selectEmail = document.getElementById("previousEmails");
+    const emailInput = document.getElementById("gmail");
+    const selectedEmail = selectEmail.options[selectEmail.selectedIndex].text;
+    emailInput.value = selectedEmail;
+}
+
+// Call the function to load previous values when the page loads
+window.addEventListener("load", loadPreviousValues);
+
+// Add event listeners to the dropdown menus
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("previousPhoneNumbers").addEventListener("change", fillPhoneNumber);
+	document.getElementById("previousEmails").addEventListener("change", fillEmail);
+  });
+  
+
+function restrictPhoneNumberInput(event) {
+	const charCode = event.which ? event.which : event.keyCode;
+	
     // Allow only numerical digits (0-9)
     if (charCode < 48 || charCode > 57) {
-      event.preventDefault();
+		event.preventDefault();
     }
-  }
+}
 
-  function displayResults() {
+
+
+
+
+function displayResults() {
 	const maxScores = [];
 	let maxScore = 0;
   
@@ -159,10 +291,21 @@ function restrictPhoneNumberInput(event) {
 		  const resultElement = document.createElement("div");
 		  resultElement.className = "result-item";
 		  resultElement.innerHTML = `
-			<img src="${category.image}" alt="${category.description}">
-			<h3>${category.name}</h3>
-			<p>${category.description}</p>
+			<img src="${category.image}" alt="${category.description}" >
 		  `;
+		  const descriptionButton = document.createElement("button");
+		  descriptionButton.className = "result-button"
+		  descriptionButton.style.backgroundColor = "transparent";
+        descriptionButton.style.borderRadius = "10px";
+        descriptionButton.style.textAlign = "center";
+        descriptionButton.style.width = "200px";
+        descriptionButton.style.margin = "20px auto 10px";
+		descriptionButton.style.color = "#fff"
+        descriptionButton.textContent = "Show Description";
+        descriptionButton.addEventListener("click", () => {
+          showDescription(category.name);
+        });
+		resultElement.appendChild(descriptionButton);
 		  resultsContainer.appendChild(resultElement);
 		}
 	  });
@@ -183,60 +326,23 @@ function restrictPhoneNumberInput(event) {
 	resultDivs.forEach((div) => {
 	  div.style.backgroundColor = "transparent";
 	  div.style.borderRadius = "10px";
-	  div.style.padding = "1rem";
 	  div.style.textAlign = "center";
 	  div.style.width = "300px";
 	});
   }
-   
+  
+  function showDescription(categoryName) {
+	// Redirect to another page with the category name as a parameter
+	const url = "description-page.html?category=" + categoryName;
+	// Open the URL in a new tab
+	window.open(url, "_blank");
+  }
+  
+  
+  
 
-// function displayResults() {
-// 	let maxScore = 0;
-// 	let maxLetter = '';
-// 	for (const letter in counts) {
-// 	  if (counts[letter] > maxScore) {
-// 		maxScore = counts[letter];
-// 		maxLetter = letter;
-// 	  }
-// 	}
-  
-// 	const category = categories.find((cat) => cat.name === maxLetter);
-  
-// 	const resultsContainer = document.getElementById('results-container');
-	
-// 	if (category) {
-// 	  const resultElement = document.createElement("div");
-// 	  resultElement.className = "result-item";
-// 	  resultElement.innerHTML = `
-// 		<img src="${category.image}" alt="${category.description}">
-// 		<h3>${category.name}</h3>
-// 		<p>${category.description}</p>
-// 	  `;
-// 	  resultsContainer.appendChild(resultElement);
-// 	} else {
-// 	  const resultElement = document.createElement("div");
-// 	  resultElement.className = "result-item";
-// 	  resultElement.textContent = "Invalid letter entered.";
-// 	  resultsContainer.appendChild(resultElement);
-// 	}
-  
-// 	// Modify styles and hide/show elements if needed
-// 	document.getElementById("heading").innerHTML = "";
-// 	document.getElementById("main-header").innerHTML = "WHAT SUITS YOU...";
-// 	document.getElementById("count").style.display = "none";
-// 	document.getElementById("hid").classList.add("hidden");
-// 	document.getElementById("container").style.height = "560px";
-  
-// 	const resultDivs = document.querySelectorAll("#results-container > div");
-// 	resultDivs.forEach((div) => {
-// 	  div.style.backgroundColor = "transparent";
-// 	  div.style.borderRadius = "10px";
-// 	  div.style.padding = "1rem";
-// 	  div.style.textAlign = "center";
-// 	  div.style.width = "300px";
-// 	});
-//   }
-  
+
+
   
     
  
